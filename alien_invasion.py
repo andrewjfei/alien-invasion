2b801ec3a27f7d1f123b5b643ffa775b5a94f8e9
andrewjfei <andrewjfei@gmail.com>
2020-09-25 13:44:03 +1200

    spaceship can move left and right on keydown and keyup events

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,17 +22,38 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_colour)
-            self.spaceship.blitme()
+    def _check_events(self):
+        # Repspond to keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the spaceship to the right.
+                    self.spaceship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the spaceship to the left.
+                    self.spaceship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.spaceship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.spaceship.moving_left = False
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+
+    def _update_screen(self):
+        # Update images on the screen, and flip to the new screen.
+        self.screen.fill(self.settings.bg_colour)
+        self.spaceship.update()
+        self.spaceship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     # Make a game instance, and run the game.
--- spaceship.py
+++ spaceship.py
@@ -15,6 +15,18 @@ class Spaceship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the spaceship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Draw the spaceship at its current location."""
         self.screen.blit(self.image, self.rect)
