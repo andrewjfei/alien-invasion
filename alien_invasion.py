979f075a1c479c0a8c70d9fadd93309d34f65363
andrewjfei <andrewjfei@gmail.com>
2020-09-25 13:13:01 +1200

    changed background colour and added spaceship image to game

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+from spaceship import Spaceship
+
 class AlienInvasion:
     """Overall class used to manage game assets and behaviour."""
 
     def __init__(self):
         """Initialise the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.spaceship = Spaceship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -20,6 +27,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_colour)
+            self.spaceship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class used to store all the settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialise the game's settings."""
+        # Screen settings.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colour = (30, 30, 30)
+        
